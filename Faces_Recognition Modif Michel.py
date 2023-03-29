8b963662bc8e75ed1c80d003b21d1b6efc90f0c3
Michel91dev <50419584+Michel91dev@users.noreply.github.com>
2023-03-29 18:25:36 +0200

    Code fonctionnel
    Incroyable, mon code a été corrigé par ChatGPT et l'a fait marcher

--- Faces_Recognition Modif Michel.py
+++ Faces_Recognition Modif Michel.py
@@ -9,43 +9,64 @@ Created on Tue Mar 28 23:15:33 2023
 
 #import the library
 import cv2
-cv2.destroyAllWindows
+#cv2.destroyAllWindows
 
 #import the cascade for face and eye
 face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 eye_cascade=cv2.CascadeClassifier("haarcascade_eye.xml")
 
+
+
 #create a function to detect the frame
-def detect(gray,frame):
-    faces=face_cascade.detectMultiScale(gray,1.3,5)
+def detect(gray,imgOrg):
+    faces = face_cascade.detectMultiScale(gray,1.3,5)
     for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0),2)
+        cv2.rectangle(imgOrg, (x,y), (x+w,y+h), (255,0,0),2)
         roi_gray=gray[y:y+h,x:x+w]
-        roi_color=frame[y:y+h,x:x+w]
+        roi_color=imgOrg[y:y+h,x:x+w]
         eyes=eye_cascade.detectMultiScale(roi_gray,1.1,2)
         for (ex,ey,eh,ew) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
-    return frame
+    return imgOrg
 
 
 
 # Read image
-img = cv2.imread('/Users/M/Documents/Dev Mike All/OpenCV Anaconda Python /COURS VIDEO/Face-Detection-Opencv (Michel)/Face-Detection-Opencv/Faces Images/Visage femme.jpg')
+img = cv2.imread("/Users/M/Documents/Dev Mike All/OpenCV Anaconda Python /COURS VIDEO/Face-Detection-Opencv (Michel)/Face-Detection-Opencv/Faces Images/Visage femme.jpg")
 
 # Convert to grayscale
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Display image
-cv2.imshow('Original Image', img)
-cv2.imshow('Grayscale Image', gray_img)
+cv2.namedWindow("Original Image", cv2.WINDOW_NORMAL) # flag to allow the window to be resizable.
 
 
-#canvas = detect(gray_img, img)
+
+cv2.imshow("Original Image", img)
+
+
+cv2.moveWindow( "Original Image" , 100,100)
+cv2.imshow("Grayscale Image", gray_img)
+
+
+w_X = cv2.getWindowImageRect("Original Image")[0] # 0=X, 1=Y, 2=width, 3=heigth
+w_width = cv2.getWindowImageRect("Original Image")[2]
+
+cv2.moveWindow( "Grayscale Image" , w_X + w_width + 3 ,100)
+
+cv2.setWindowProperty("Original Image", cv2.WND_PROP_TOPMOST, 1) # censé mettre l'iamge original devant car elle apparait derrière Spyder ?!
+
+
+
+
+canvas = detect(gray_img, img)
+    cv2.imshow("Image avec visages" , canvas)
+
 
 cv2.waitKey(0)
 
 #while not(cv2.waitKey(1) & 0XFF==ord("q")):
  #       break
 #video.release()
-cv2.destroyAllWindows
+cv2.destroyAllWindows()
                       
