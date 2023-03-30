d611d93ce98ee19282eaa95729f4b46b65961bc0
Michel91dev <50419584+Michel91dev@users.noreply.github.com>
2023-03-30 03:29:21 +0200

    Almost done but...
    Bug persist in the 3rd Window positioning. Not sure why this happens...

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ img = cv2.imread("Faces Images/Visage femme.jpg")
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Display image
-cv2.namedWindow("Original Image", cv2.WINDOW_NORMAL)  # flag to allow the window to be resizable.
+#cv2.namedWindow("Original Image", cv2.WINDOW_NORMAL)  # flag to allow the window to be resizable.
 cv2.imshow("Original Image", img)
 
 cv2.moveWindow("Original Image", 100, 100)
@@ -43,17 +43,18 @@ cv2.imshow("Grayscale Image", gray_img)
 w_X = cv2.getWindowImageRect("Original Image")[0]  # 0=X, 1=Y, 2=width, 3=height
 w_width = cv2.getWindowImageRect("Original Image")[2]
 
-# et l'autre à coté
+# et la deuxième à coté
 cv2.moveWindow("Grayscale Image", w_X + w_width + 3, 100)
 
-canvas = detect(gray_img, img)
+canvas = detect(gray_img, img) # ▶️
 
 cv2.imshow("Image avec visages", canvas)
 
 # Move the "Image avec visages" window next to the "Grayscale Image" window
-gray_w_X = cv2.getWindowImageRect("Grayscale Image")[0]
+gray_w_X = cv2.getWindowImageRect("Grayscale Image")[0] # 🔴 bug ou erreur, mais la valeur est de 1606 ce qui n'est pas logique puisque la fenetre 2 a x=200 (et width=600)
 gray_w_width = cv2.getWindowImageRect("Grayscale Image")[2]
 
+#cv2.moveWindow("Image avec visages", w_X * 2 + gray_w_width * 2 + 3, 100)
 cv2.moveWindow("Image avec visages", gray_w_X + gray_w_width + 3, 100)
 
 cv2.waitKey(0)
